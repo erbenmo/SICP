e64a325f7114cfb6ac85fc8e69dd69a6eaa37711
Erben Mo <erbenmo@gmail.com>
2012-05-05 18:01:29 +0800

    1.35-1.37 before dinner...

---
+++ Chapter 1/1-37.scm
@@ -0,0 +1,21 @@
+(define (cont-frac n d k)
+  (cont-frac-rec n d k 1))
+
+;; 4 decimal accuracy in 11 steps
+(define (cont-frac-rec n d k cur)
+  (if (or (> cur k) (= cur k))
+      (/ (n cur) (d cur))
+      (/ (n cur) (+ (d cur) (cont-frac-rec n d k (+ cur 1))))))
+
+(define (cont-iter n d k)
+  (cont-frac-iter n d k k 0))
+
+;;; ???
+(define (cont-frac-iter n d k cur result)
+  (cond
+   ((= cur k)
+    (cont-frac-iter n d k (- cur 1) (/ (n cur) (d cur))))
+   ((= cur 0)
+    result)
+   (else
+    (cont-frac-iter n d k (- cur 1) (/ (n cur) (+ (d cur) result))))))
